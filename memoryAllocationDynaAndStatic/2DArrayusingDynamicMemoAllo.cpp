f9087449112e43c64f84c9667254455940d31b2a
OsmanXwiinder <osmansaifi30@gmail.com>
2025-03-09 01:07:42 -0800

    dynamicmemo

--- memoryAllocationDynaAndStatic/2DArrayusingDynamicMemoAllo.cpp
+++ memoryAllocationDynaAndStatic/2DArrayusingDynamicMemoAllo.cpp
@@ -2,7 +2,33 @@
 using namespace std;
 
 int main(){
+    int row;
+    cin >> row;
+    int col;
+    cin >> col;
+
+    int **arr = new int *[row]; // this is ptr or total row in matrix
+    for(int i=0; i<row; i++){
+        arr[i] = new int[col];  //  this is a every row size
+    }
+
+    //taking intput
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cin >> arr[i][j];
+        }
+    }
+    cout << endl;
     
+    // print 
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
 
 
     return 0;
