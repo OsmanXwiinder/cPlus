be2fd0f0afbe55bbef658f85d6ee0da93bdd2848
OsmanXwiinder <osmansaifi30@gmail.com>
2025-03-23 22:44:40 +0530

    new

--- ClassesAndObjects/copyConstructor.cpp
+++ ClassesAndObjects/copyConstructor.cpp
@@ -9,22 +9,27 @@ class Car{
     Car(string name, string color){
         this->name = name;
         this->color = color;
-        mileage = new int;
+        mileage = new int; 
         *mileage = 12;
     }
     Car(Car &original){
         cout << "copying original to new" << endl;
          name = original.name;
-         color = original.name; 
+         color = original.name;
+         mileage = new int; // for a deep copy we allocate again dynamically
+         *mileage = *original.mileage; // address ye bhi   ye mileage kha point karega jha 
+                                    // *original.mileage karega to actaul copy ho jaygi
     }
 };
 
 int main(){
-    Car s1("suzuki","black");
+    Car c1("suzuki","black");
 
-    Car s2(s1);
-    Car s3(s2);
-    cout << "s2 name => " << s3.name << endl;
-    cout << "s2 color => " << s3.color << endl;
+    Car c2(c1);  
+    cout << "s2 name => " << c2.name << endl;
+    cout << "s2 color => " << c2.color << endl; 
+    cout << *c2.mileage << endl;
+    *c2.mileage = 10;
+    cout << *c1.mileage << endl; // koi change nhi abb 
     return 0;
 } 
