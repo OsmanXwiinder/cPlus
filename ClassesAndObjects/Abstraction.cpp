6e84b2d5775990b1fb073842e063e5796ab42fb9
OsmanXwiinder <osmansaifi30@gmail.com>
2025-03-26 15:22:43 +0530

    new Commit

--- ClassesAndObjects/Abstraction.cpp
+++ ClassesAndObjects/Abstraction.cpp
@@ -2,8 +2,37 @@
 using namespace std;
 
 
-int main(){
+//{abstract class}
+class Shap{
+    public:
+    virtual void draw() = 0;  //{pure virtual function}
+};  
 
+class Circle : public Shap {
+    public:
+    void draw(){
+        cout << "Draw cirlce\n";
+    }
+    
+};
+
+class Squar : public Shap {
+    public:
+    void draw(){
+        cout << "Draw squar\n";
+    }
+};    
+
+
+int main(){
+    Circle cir1;
+    cir1.draw();
+
+    Squar Squ1;
+    Squ1.draw();
+
+    Shap a1;
+    a1.draw();
 
     return 0;
 }
---
+++ ClassesAndObjects/StaticKeyword.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// void counter(){
+//     static int count = 0;
+//     count++;
+//     cout << "count : " << count << endl;
+// }   
+ 
+class Example{
+    public: 
+    static int x;
+
+};
+
+int Example::x = 0;
+
+int main(){ 
+    Example eg1;
+    Example eg2;
+    Example eg3;
+
+    cout << eg1.x++ << endl;
+    cout << eg2.x++ << endl;
+    cout << eg3.x++ << endl;
+
+    return 0;
+}
