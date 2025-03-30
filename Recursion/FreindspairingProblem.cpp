aedf00ba06a2af4614d77ef6f0c689ad45e9ada2
OsmanXwiinder <osmansaifi30@gmail.com>
2025-03-30 15:37:13 +0530

    new Questions

--- Recursion/FreindspairingProblem.cpp
+++ Recursion/FreindspairingProblem.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 
-int main(){
-        
+int FreindsPairing(int n){
+    // here is n = is ways if n-> 1 so return s1 way and if 2 so return 2;
+    if(n == 1 || n == 2){   
+        return n;
+    }
 
+    // single
+   int single = FreindsPairing(n-1);
+    //pair 
+   int pair = (n-1) * FreindsPairing(n-2);
+
+   return single+pair;
+}
+
+int main(){
+     int ans = FreindsPairing(3);
+    cout << ans << endl;
     return 0;
 }
